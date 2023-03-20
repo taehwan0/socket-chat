ca04d6c3a48196bedc509201d62d5dc7e4dd7e8e
buddy <jang_taehwan@naver.com>
2023-03-20 22:40:51 +0900

    git init

---
+++ src/test/java/com/example/socketchat/SocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
